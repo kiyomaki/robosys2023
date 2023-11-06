890616affe044860375990711c17e7509c0f8e87
Maxi Kiyosawa <s22C1040UT@s.chibakoudai.jp>
2023-11-06 20:18:51 +0900

    change test.bash and plus

--- test.bash
+++ test.bash
@@ -1,5 +1,16 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 Ryuichi Ueda
+# SPDX-License-Identifier: BSD-3-Clause
+ng() {
+      echo NG at Line $1
+      res=1
+}
 
-seq 5 | ./plus
+res=0
 
-[ "${out}" = 14 ]
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+echo $out
+[ "${out}" = 15 ] || ng ${LINENO}
+[ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
+exit $res
